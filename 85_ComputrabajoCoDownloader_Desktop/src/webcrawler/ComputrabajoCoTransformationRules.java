11979320861a4c188f5a4186ff143310bcd480ff
Oscar <jedilink@gmail.com>
2016-01-21 12:14:19 -0500

    2h13min: Revisión de errores en el proceso de importación para el caso de elementos de fecha y número mal importados por la herramienta de extracción.

--- 85_ComputrabajoCoDownloader_Desktop/src/webcrawler/ComputrabajoCoTransformationRules.java
+++ 85_ComputrabajoCoDownloader_Desktop/src/webcrawler/ComputrabajoCoTransformationRules.java
@@ -24,8 +24,8 @@ import vsdk.toolkit.common.VSDK;
 public class ComputrabajoCoTransformationRules {
 
     private static final ComputrabajoDatabaseConnection databaseConnection;
-    private static DBCollection professionalResume;
-    private static DBCollection professionalResumeTrans;
+    private static final DBCollection professionalResume;
+    private static final DBCollection professionalResumeTrans;
 
     static {
         databaseConnection = new ComputrabajoDatabaseConnection(
@@ -39,7 +39,7 @@ public class ComputrabajoCoTransformationRules {
      * @param name
      * @return
      */
-    public static String transformationName(String name) {
+    public static String transformName(String name) {
         name = name.replaceAll("\\s\\s*", " ");
         name = name.toLowerCase();
         String[] nameAsArray = name.split(" ");
@@ -60,7 +60,7 @@ public class ComputrabajoCoTransformationRules {
         return nameTrans;
     }
 
-    public static String ValidateEmail(String email) {
+    public static String validateEmail(String email) {
         Pattern pat = Pattern.compile(
             "^[\\w-]+(\\.[\\w-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
         Matcher mat = pat.matcher(email);
@@ -72,7 +72,7 @@ public class ComputrabajoCoTransformationRules {
         return null;
     }
 
-    public static String TransformationMonth(String _date) {
+    public static String transformMonth(String _date) {
         String date;
         String[] dateAux = _date.split(" de ");
 
@@ -130,11 +130,11 @@ public class ComputrabajoCoTransformationRules {
         return date;
     }
 
-    public static String TransformationDate(String date) {
+    public static String transformDate(String date) {
         String dateTrans = null;
         if (date != null) {
             date = date.replaceAll("\\s\\s*", " ");
-            String dateAux = TransformationMonth(date.trim());
+            String dateAux = transformMonth(date.trim());
 
             if (dateAux != null) {
                 dateTrans = dateAux;
@@ -146,7 +146,7 @@ public class ComputrabajoCoTransformationRules {
         return dateTrans;
     }
 
-    public static Date transformationDateSimple(String date) {
+    public static Date transformSimpleDate(String date) {
         Date dateTrans = null;
         if (date != null) {
             date = date.trim();
@@ -163,8 +163,8 @@ public class ComputrabajoCoTransformationRules {
         return dateTrans;
     }
 
-    public static String TransformationProfession(String html) {
-        String profession = "";
+    public static String transformProfession(String html) {
+        String profession;
 
         String[] stringAux = html.split("</H2></LI>");
 
@@ -178,7 +178,7 @@ public class ComputrabajoCoTransformationRules {
         return profession.trim();
     }
 
-    public static Double TransformationPayment(String _pay) {
+    public static Double trasnsformPayment(String _pay) {
         Pattern pat = Pattern.compile("^[0-9.,]*");
         String[] payAsArray = _pay.split(" ");
         Double pay = 0.0;
@@ -196,7 +196,7 @@ public class ComputrabajoCoTransformationRules {
         return pay;
     }
 
-    public static void insertMongo(DBCursor c) {
+    public static void processAllRegisters(DBCursor c) {
         SimpleDateFormat date;
         date = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
         Date transformationDate = new Date();
@@ -212,22 +212,31 @@ public class ComputrabajoCoTransformationRules {
             count++;
             
             ei = c.next();
-            r.importMongoFields(ei);
+            if ( !r.importMongoFields(ei) ) {
+                System.out.println(
+                    "Warning: Error importing object with _id: " + r.get_id());
+            }
 
             try {
                 searchQuery.append("_id", r.get_id());
-                searchQuery.append("name", transformationName(r.getName()));
+                searchQuery.append("name", transformName(r.getName()));
                 searchQuery.append("documentId", r.getDocumentId());
                 searchQuery.append("location", r.getLocation());
                 searchQuery.append("country", r.getCountry());
-                searchQuery.append("email", ValidateEmail(r.getEmail()));
+                searchQuery.append("email", validateEmail(r.getEmail()));
                 searchQuery.append("emailStatus", r.getEmailStatus());
+                
                 searchQuery.append("phone", r.getPhone());
                 try {
                     searchQuery.append("lastUpdateDate", 
-                        date.parse(TransformationDate(r.getLastUpdateDate())));
-                } catch (Exception e) {
-                    System.out.println(r.get_id());
+                        date.parse(transformDate(r.getLastUpdateDate())));
+                    searchQuery.append("registrationDate", 
+                        date.parse(transformDate(r.getRegistrationDate())));
+                    searchQuery.append("lastLoginDate", 
+                        date.parse(transformDate(r.getLastLoginDate())));
+                } 
+                catch ( Exception e ) {
+                    System.out.println("Warning: Error importing object with _id:" + r.get_id());
                 }
                 searchQuery.append("profilePictureUrl", 
                     r.getProfilePictureUrl());
@@ -235,28 +244,24 @@ public class ComputrabajoCoTransformationRules {
                 searchQuery.append("pair", r.getPair());
                 searchQuery.append("jobSearchStatus", r.getJobSearchStatus());
                 searchQuery.append("wantedPayment", 
-                    TransformationPayment(r.getWantedPayment()));
+                    trasnsformPayment(r.getWantedPayment()));
                 searchQuery.append("descriptionTitle", 
                     r.getDescriptionTitle());
                 searchQuery.append("resumeLink", 
                     r.getResumeLink());
                 searchQuery.append("profession", 
-                    TransformationProfession(r.getHtmlContent()));
-                searchQuery.append("registrationDate", 
-                    date.parse(TransformationDate(r.getRegistrationDate())));
-                searchQuery.append("lastLoginDate", 
-                    date.parse(TransformationDate(r.getLastLoginDate())));
+                    transformProfession(r.getHtmlContent()));
                 searchQuery.append("sourceUrl", r.getSourceUrl());
                 searchQuery.append("transformationDate", transformationDate);
 
-                if (professionalResumeTrans.findOne(r.get_id()) == null) {
+                if ( professionalResumeTrans.findOne(r.get_id()) == null ) {
                     professionalResumeTrans.insert(searchQuery);
                 }
-            } catch (ParseException e) {
-                e.printStackTrace();
-            } catch (java.text.ParseException e) {
-                e.printStackTrace();
-            }
+            } 
+            catch ( ParseException e ) {
+                System.out.println(
+                    "Warning: Error importing object with _id: " + r.get_id());
+            } 
         }
     }
 
@@ -265,7 +270,7 @@ public class ComputrabajoCoTransformationRules {
         c = professionalResume.find();
 
         if (c != null) {
-            insertMongo(c);
+            processAllRegisters(c);
         }
     }
 }
