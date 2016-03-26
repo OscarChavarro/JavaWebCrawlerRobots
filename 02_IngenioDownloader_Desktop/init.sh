c00901deec770713fe50ac78c4684202b75cca0b
SithMasterBDSM <jedilink@gmail.com>
2016-03-26 04:31:14 -0500

    0h03min: scripts updated

---
+++ 02_IngenioDownloader_Desktop/init.sh
@@ -0,0 +1,32 @@
+mkdir 82_IngenioDownloader_Desktop
+cd 82_IngenioDownloader_Desktop
+mkdir lib src classes
+cp ~/JavaWebCrawlerRobots/02_IngenioDownloader_Desktop/compile.sh .
+cp ~/JavaWebCrawlerRobots/02_IngenioDownloader_Desktop/run.sh .
+cd src
+ln -s ~/JavaWebCrawlerRobots/01_JsonLib_Desktop/src/org .
+mkdir databaseConnection  databaseMongo  webcrawler
+cd databaseConnection
+ln -s  ~/JavaWebCrawlerRobots/01_Utilities/src/databaseConnection/* .
+cd ..
+cd databaseMongo/
+mkdir model
+cd model/
+ln -s  ~/JavaWebCrawlerRobots/01_Utilities/src/databaseMongo/model/JdbcEntity.java .
+cd ../..
+cd webcrawler/
+ln -s  ~/JavaWebCrawlerRobots/01_Utilities/src/webcrawler/* .
+cd ..
+ln -s ~/JavaWebCrawlerRobots/10_VitralLib_Desktop/src/vsdk/ .
+ls ~/JavaWebCrawlerRobots/82_IngenioDownloader_Desktop/src/
+cd databaseMongo/
+ln -s ~/JavaWebCrawlerRobots/02_IngenioDownloader_Desktop/src/databaseMongo/*.java .
+cd model/
+ln -s ~/JavaWebCrawlerRobots/02_IngenioDownloader_Desktop/src/databaseMongo/model/* .
+cd ../../webcrawler/
+ln -s ~/JavaWebCrawlerRobots/02_IngenioDownloader_Desktop/src/webcrawler/* .
+cd ../..
+cd lib/
+ln -s ~/JavaWebCrawlerRobots/_commonLibsInJar/* .
+cd ..
+./compile.sh
--- 85_ComputrabajoCoDownloader_Desktop/run.sh
+++ 85_ComputrabajoCoDownloader_Desktop/run.sh
@@ -4,6 +4,6 @@ if [ $? -eq 0 ]; then
     exit
 fi
 
-nohup time java -Xmx1512m -Xms1512m -classpath ./classes:./lib/10_VitralLib_Desktop.jar:./lib/commons-logging-1.2.jar:./lib/httpclient-4.4.1.jar:./lib/httpcore-4.4.1.jar:./lib/httpmime-4.4.1.jar:./lib/mongo-java-driver-2.13.2.jar webcrawler.Tool01_ExtractionDownloader &> log.txt &
+nohup time java -Xmx800m -Xms800m -classpath ./classes:./lib/10_VitralLib_Desktop.jar:./lib/commons-logging-1.2.jar:./lib/httpclient-4.4.1.jar:./lib/httpcore-4.4.1.jar:./lib/httpmime-4.4.1.jar:./lib/mongo-java-driver-2.13.2.jar webcrawler.Tool01_ExtractionDownloader &> log.txt &
 disown
 echo Process started. Use command tail -f log.txt to see advances.
